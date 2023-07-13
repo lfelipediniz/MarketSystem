d63db7259f271b0a9d2a9d0af8f1ccf9ad9abb39
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-07-13 14:05:57 -0300

    work almost finished, need to check the problem with Balance

--- main.c
+++ main.c
@@ -3,160 +3,189 @@
 #include <string.h>
 
 typedef struct {
-    // struct do Produto
-    unsigned int id;
-    char name[80];
-    unsigned int qtd;
-    float price;
+   unsigned int id;
+   char name[80];
+   unsigned int qtd;
+   float price;
 } Product;
 
 typedef struct {
-    // struct do Estoque
-
-    unsigned int qtd;
-    unsigned int capacity;
-    Product *products;
+   unsigned int qtd;
+   unsigned int capacity;
+   Product *products;
 } Stock;
 
 void insertProduct(Stock *storage);
 void increaseStock(Stock *storage);
 void modifyPrice(Stock *storage);
-float sale(Stock *storage);
+void sale(Stock *storage);
 void inventoryConsultation(Stock *storage);
+void checkBalance(float *cash);
+void finalizeDay(Stock *storage, float cash);
 
+// inserir um produto no estoque
 void insertProduct(Stock *storage) {
-    char name[80];
-    unsigned int qtd;
-    float price;
+   char name[80];
+   unsigned int qtd;
+   float price;
 
-    scanf("%s", name);
-    scanf("%u", &qtd);
-    scanf("%f", &price);
+   scanf("%s", name);
+   scanf("%u", &qtd);
+   scanf("%f", &price);
 
-    if (storage->qtd == storage->capacity) {
-        storage->products = (Product *)realloc(storage->products, storage->capacity * 2);
-        storage->capacity *= 2;
-    }
+   // vê se o estoque está cheio e realoca espaço quando necessário
+   if (storage->qtd == storage->capacity) {
+      storage->products = realloc(storage->products, (storage->capacity + 10) * sizeof(Product));
+      storage->capacity += 10;
+   }
 
-    strcpy(storage->products[storage->qtd].name, name);
-    storage->products[storage->qtd].qtd = qtd;
-    storage->products[storage->qtd].price = price;
-    storage->products[storage->qtd].id = storage->qtd;
+   // Insere o produto no estoque
+   strcpy(storage->products[storage->qtd].name, name);
+   storage->products[storage->qtd].qtd = qtd;
+   storage->products[storage->qtd].price = price;
+   storage->products[storage->qtd].id = storage->qtd;
 
-    storage->qtd++;
+   storage->qtd++;
 }
 
+// aumenta o estoque de um produto
 void increaseStock(Stock *storage) {
-    unsigned int id, qtd;
-    scanf("%u %u", &id, &qtd);
-    storage->products[id].qtd += qtd;
+   unsigned int id, qtd;
+   scanf("%u %u", &id, &qtd);
+   storage->products[id].qtd += qtd;
 }
 
+// modificar o preço de um produto
 void modifyPrice(Stock *storage) {
-    unsigned int id;
-    float prc;
-    scanf("%u %f", &id, &prc);
-    storage->products[id].price = prc;
+   unsigned int id;
+   float prc;
+   scanf("%u %f", &id, &prc);
+   storage->products[id].price = prc;
 }
 
-//Não coloquei para printar codigo de erro pq as saidas tem que ser exatamente igual o esperado
-//A função retorna o saldo depois das vendas, para facilitar na hora de fazer o comando CS
-float sale(Stock *storage) {
-    int id = 0;
-    float saldo = 0;
-    float dinheiroVenda = 0;
+// fazer a venda
+void sale(Stock *storage) {
+   int id = 0;
+   float total = 0;
 
-    while (1) {
-        scanf("%d", &id);
+   // Loop para ler os códigos dos produtos vendidos
+   while (1) {
+      scanf("%d", &id);
 
-        if (id == -1)
-            break;
-        else if (id >= 0 && storage->products[(unsigned) id].qtd > 0 && id < (int)storage->qtd) {
-            storage->products[(unsigned) id].qtd -= 1;
-            dinheiroVenda += storage->products[(unsigned) id].price;
-            printf("%s %.2f \n", storage->products[id].name, storage->products[id].price);
-            saldo += storage->products[id].price * (float) storage->products[id].qtd;
-        }
-    }
+      if (id == -1)
+         break;
+      else if (id >= 0 && id < storage->qtd && storage->products[id].qtd > 0) {
+         printf("%s %.2f\n", storage->products[id].name, storage->products[id].price);
+         total += storage->products[id].price;
+         storage->products[id].qtd--;
+      }
+   }
 
-    printf("Total: %.2f\n", dinheiroVenda);
+   printf("Total: %.2f\n", total);
+   for (int j = 0; j < 50; j++) printf("-");
 
-    for (int j = 0; j < 50; j++)
-        printf("-");
-
-    printf("\n");
-
-    return saldo;
+   printf("\n");
 }
 
+// consulta o estoque
 void inventoryConsultation(Stock *storage) {
-    int i;
+   for (int i = 0; i < storage->qtd; i++) {
+      printf("%u %s %u\n", storage->products[i].id, storage->products[i].name, storage->products[i].qtd);
+   }
 
-    for (i = 0; i < (int)storage->qtd; i++) {
-        printf("%u %s %u \n", storage->products[i].id, storage->products[i].name, storage->products[i].qtd);
-    }
+   for (int j = 0; j < 50; j++) printf("-");
 
-    for (int j = 0; j < 50; j++)
-        printf("-");
+   printf("\n");
+}
 
-    printf("\n");
+// consulta o saldo do caixa
+void checkBalance(float *cash) {
+   scanf("%f", cash);
+}
+
+// finaliza o dia e grava as informações no arquivo binário
+void finalizeDay(Stock *storage, float cash) {
+   FILE *file = fopen("dados.bin", "wb");
+
+   if (file == NULL) {
+      printf("Erro ao abrir o arquivo.\n");
+      return;
+   }
+
+   // grava o saldo e a quantidade de produtos no arquivo
+   fwrite(&cash, sizeof(float), 1, file);
+   fwrite(&(storage->qtd), sizeof(unsigned int), 1, file);
+
+   // grava os produtos no arquivo
+   fwrite(storage->products, sizeof(Product), storage->qtd, file);
+
+   fclose(file);
 }
 
 int main() {
-    FILE *fp;
-    unsigned int stockSize; //primeiro número do arquivo
-    int balance; //segundo número do arquivo
-    char command[3];
-    Stock stockStorage;
+   unsigned int stockSize;
+   char command[3];
+   Stock stockStorage;
+   float cash;
 
-    fp = fopen("MarketFile", "r+");
+   FILE *file = fopen("dados.bin", "rb");
 
-    // checar se existe o arquivo do dia anterior
-    if(fp == NULL){
-        fp = fopen("MarketFile","w");
-        scanf("%u", &stockSize);
-        scanf("%d", &balance);
+   if (file != NULL) {
+      // lê o saldo e a quantidade de produtos do arquivo
+      fread(&cash, sizeof(float), 1, file);
+      fread(&stockSize, sizeof(unsigned int), 1, file);
 
-        // só para ser possível alocar a memoria nos testes, deve ser atualizado com os dados do fim do dia
-        fprintf(fp, "%u %d", stockSize, balance);
-    }else{
-        fscanf(fp, "%u %d", &stockSize, &balance);
-    }
+      // aloca a memória pro estoque
+      stockStorage.products = (Product *)malloc(stockSize * sizeof(Product));
+      if (stockStorage.products == NULL) {
+         printf("Erro na alocacao de memoria.\n");
+         return 1;
+      }
 
-    // alocação dinâmica do estoque que contém os produtos
-    stockStorage.products = (Product *) malloc(stockSize * sizeof(Product));
-    if (stockStorage.products == NULL) {
-        printf("Erro na alocacao de memoria.\n");
-        return 1;
-    }
+      stockStorage.capacity = stockSize;
+      stockStorage.qtd = fread(stockStorage.products, sizeof(Product), stockSize, file);
 
-    stockStorage.capacity = stockSize;
-    stockStorage.qtd = 0;
+      fclose(file);
+   } else {
+      // se o arquivo não existe, lê o espaço atual do estoque do console
+      scanf("%u", &stockSize);
 
-    // loop infinito dos comandos, para só quando o usuário digitar "FE"
-    // (Finalizar Execução)
-    while (1) {
-        //colocar o comando
-        scanf("%s", command);
+      // aloca memória pro estoque
+      stockStorage.products = (Product *)malloc(stockSize * sizeof(Product));
+      if (stockStorage.products == NULL) {
+         printf("Erro na alocacao de memoria.\n");
+         return 1;
+      }
 
-        if (strcmp(command, "IP") == 0) {
-            insertProduct(&stockStorage);
-        } else if (strcmp(command, "AE") == 0) {
-            increaseStock(&stockStorage);
-        } else if (strcmp(command, "MP") == 0) {
-            modifyPrice(&stockStorage);
-        } else if (strcmp(command, "VE") == 0) {
-            sale(&stockStorage);
-        } else if (strcmp(command, "CE") == 0) {
-            inventoryConsultation(&stockStorage);
-        } else if (strcmp(command, "FE") == 0) {
-            break;  // sai do loop
-        }
-    }
+      stockStorage.capacity = stockSize;
+      stockStorage.qtd = 0;
+   }
 
-    // libera a memória alocada para o estoque
-    free(stockStorage.products);
+   while (1) {
+      scanf("%s", command);
 
-    fclose(fp);
-    return 0;
+      if (strcmp(command, "IP") == 0) {
+         insertProduct(&stockStorage);
+      } else if (strcmp(command, "AE") == 0) {
+         increaseStock(&stockStorage);
+      } else if (strcmp(command, "MP") == 0) {
+         modifyPrice(&stockStorage);
+      } else if (strcmp(command, "VE") == 0) {
+         sale(&stockStorage);
+      } else if (strcmp(command, "CE") == 0) {
+         inventoryConsultation(&stockStorage);
+      } else if (strcmp(command, "CS") == 0) {
+         checkBalance(&cash);
+         printf("Saldo: %.2f\n", cash);
+         for (int j = 0; j < 50; j++) printf("-");
+         printf("\n");
+      } else if (strcmp(command, "FE") == 0) {
+         finalizeDay(&stockStorage, cash);
+         break;
+      }
+   }
+
+   free(stockStorage.products);
+
+   return 0;
 }
